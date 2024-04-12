efd3a8cb144a3fcf2d2014c79a82569742123df0
Xavier KiChi Mouat <Xavier@Xaviers-MacBook-Pro.local>
2024-04-12 22:44:11 +1000

    basic fetch working, returning json

--- login.py
+++ login.py
@@ -1,5 +1,6 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from urllib.parse import unquote
+import json
 
 import dbCreate
 
@@ -49,34 +50,64 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.wfile.write(bytes('Something f**ked up! Sorry.', 'utf-8'))
 
     def do_POST(self):
-        try:
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            content_len = int(self.headers.get('content-length'))
-            #returns a bytes type, NOT string!!
-            postbody = self.rfile.read(content_len)
-            #https://www.freecodecamp.org/news/python-bytes-to-string-how-to-convert-a-bytestring/
-            #https://stackoverflow.com/questions/33143504/how-do-i-encode-decode-percent-encoded-url-strings-in-python
-            actualRequestLine = unquote(postbody.decode('utf-8'))
-            formKeyValues = splitFormValues(actualRequestLine)
-            '''
-            for key in formKeyValues.keys():
-                print(key, ' = ', formKeyValues[key])
-            '''
-            response = '<div style="color: '
-            response += formKeyValues['faveColour']
-            response += ';">hello '
-            response += formKeyValues['username']
-            response += ', your favourite animal is a '
-            response += formKeyValues['animalSize'] + ' '
-            response += formKeyValues['faveAnimal'] + '</div>'
-            self.wfile.write(bytes(response, 'utf-8'))
-        except:
-            self.send_response(404)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            self.wfile.write(bytes('Something f**ked up in POST! Sorry.', 'utf-8'))
+        #https://www.freecodecamp.org/news/python-switch-statement-switch-case-example/
+        match self.path:
+            case '/':
+                try:
+                    self.send_response(200)
+                    self.send_header('Content-type', 'text/html')
+                    self.end_headers()
+                    content_len = int(self.headers.get('content-length'))
+                    #returns a bytes type, NOT string!!
+                    postbody = self.rfile.read(content_len)
+                    #https://www.freecodecamp.org/news/python-bytes-to-string-how-to-convert-a-bytestring/
+                    #https://stackoverflow.com/questions/33143504/how-do-i-encode-decode-percent-encoded-url-strings-in-python
+                    actualRequestLine = unquote(postbody.decode('utf-8'))
+                    #print(actualRequestLine)
+                    formKeyValues = splitFormValues(actualRequestLine)
+                    '''
+                    for key in formKeyValues.keys():
+                        print(key, ' = ', formKeyValues[key])
+                    '''
+                    response = '<div style="color: '
+                    response += formKeyValues['faveColour']
+                    response += ';">hello '
+                    response += formKeyValues['username']
+                    response += ', your favourite animal is a '
+                    response += formKeyValues['animalSize'] + ' '
+                    response += formKeyValues['faveAnimal'] + '</div>'
+                    self.wfile.write(bytes(response, 'utf-8'))
+                except:
+                    self.send_response(404)
+                    self.send_header('Content-type', 'text/html')
+                    self.end_headers()
+                    self.wfile.write(bytes('Something f**ked up in POST! Sorry.', 'utf-8'))
+
+            case '/checkUsernameExists':
+                try:
+                    self.send_response(200)
+                    self.send_header('Content-type', 'text/javascript')
+                    self.end_headers()
+                    content_len = int(self.headers.get('content-length'))
+                    #returns a bytes type, NOT string!!
+                    postbody = self.rfile.read(content_len)
+                    #https://www.freecodecamp.org/news/python-bytes-to-string-how-to-convert-a-bytestring/
+                    #https://stackoverflow.com/questions/33143504/how-do-i-encode-decode-percent-encoded-url-strings-in-python
+                    actualRequestLine = unquote(postbody.decode('utf-8'))
+                    formKeyValues = splitFormValues(actualRequestLine)
+
+                    response = json.dumps({
+                        "usernameExists": "false"
+                    })
+                    self.wfile.write(bytes(response, 'utf-8'))
+                except:
+                    self.send_response(404)
+                    self.send_header('Content-type', 'text/html')
+                    self.end_headers()
+                    self.wfile.write(bytes('Something f**ked up in POST! Sorry.', 'utf-8'))
+
+            case _:
+                print('default case')
 
 httpd = HTTPServer(('', 8000), SimpleHTTPRequestHandler)
 httpd.serve_forever()
